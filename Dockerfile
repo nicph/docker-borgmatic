9762a28d5b1664567d6a02310f48c1cf63da996a
N <nicph@users.noreply.github.com>
2019-06-09 19:28:27 +0200

    Add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+FROM alpine AS borg
+
+ARG BORG_VERSION=1.1.10
+ARG BORGMATIC_VERSION=1.3.5
+
+# https://borgbackup.readthedocs.io/en/stable/installation.html#dependencies
+RUN apk add --upgrade --no-cache \
+      acl-dev \
+      attr-dev \
+      build-base \
+      fuse-dev \
+      python3-dev \
+      linux-headers \
+      openssl-dev
+
+RUN python3 -m venv /app && /app/bin/pip install \
+      borgbackup==${BORG_VERSION} \
+      borgmatic==${BORGMATIC_VERSION} \
+      llfuse \
+      prometheus_client
+
+
+
+FROM alpine
+MAINTAINER nicph
+
+RUN apk --update --no-cache add \
+      libacl \
+      libattr \
+      fuse \
+      openssl \
+      python3 \
+      openssh-client
+
+COPY --from=borg /app /app
+COPY env.sh /etc/profile.d/
+COPY entrypoint borg_exporter /app/bin/
+
+ENV BORG_BASE_DIR=/borg
+
+VOLUME /borg
+VOLUME /etc/borgmatic.d
+
+ENTRYPOINT ["/app/bin/entrypoint"]
---
+++ env.sh
@@ -0,0 +1,41 @@
+export PATH="/app/bin:${PATH}"
+
+# set some default values for convenience.
+# BORG_* are borg variables, see
+# https://borgbackup.readthedocs.io/en/stable/usage/general.html#environment-variables
+#
+# SSH_KEY_FILE, SSH_KNOWN_HOSTS_FILE and SSH_OPTS are convenience variables
+# used to build BORG_RSH
+
+export BORG_BASE_DIR="${BORG_BASE_DIR:-/borg}"
+
+export BORG_CACHE_DIR="${BORG_CACHE_DIR:-${BORG_BASE_DIR}/cache}"
+export BORG_CONFIG_DIR="${BORG_CONFIG_DIR:-${BORG_BASE_DIR}/config}"
+export BORG_KEYS_DIR="${BORG_SECURITY_DIR:-${BORG_BASE_DIR}/keys}"
+export BORG_SECURITY_DIR="${BORG_SECURITY_DIR:-${BORG_BASE_DIR}/security}"
+
+BORG_KEY_FILE="${BORG_KEY_FILE:-/run/secrets/borg-key}"
+if [ -r "$BORG_KEY_FILE" ]; then
+  export BORG_KEY_FILE
+fi
+
+
+SSH_KEY_FILE="${SSH_KEY_FILE:-/run/secrets/ssh-key}"
+if [ -r "$SSH_KEY_FILE" ]; then
+  SSH_OPTS="${SSH_OPTS} -i '${SSH_KEY_FILE}'"
+fi
+
+SSH_KNOWN_HOSTS_FILE="${SSH_KNOWN_HOSTS_FILE:-${BORG_BASE_DIR}/known_hosts}"
+if [ -r "$SSH_KNOWN_HOSTS_FILE" ]; then
+  SSH_OPTS="${SSH_OPTS} -o 'UserKnownHostsFile=${SSH_KNOWN_HOSTS_FILE}'"
+fi
+
+if [ -n "$SSH_OPTS" ]; then
+  export BORG_RSH="${BORG_RSH:-ssh} ${SSH_OPTS}"
+fi
+
+export IMAGE_CRONTAB_FILE="${IMAGE_CRONTAB_FILE:-/etc/borgmatic.d/crontab}"
+
+# IMAGE_EXPORTER_PORT=   # no default value, defining it enables the exporter service
+IMAGE_METRICS_DIR=${IMAGE_METRICS_DIR:-/prometheus}
+IMAGE_METRICS_FILENAME=${IMAGE_METRICS_FILENAME:-metrics}
